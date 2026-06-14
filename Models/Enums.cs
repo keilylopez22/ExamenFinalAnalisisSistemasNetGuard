f07537a66f81e98cb432cc2ddf60e8c14610febb
KeilyLopez <klopezh16@miumg.edu.gt>
2026-06-13 22:47:06 -0600

    correccion estructura de proyecto
    eliminar archivos duplicados

--- Models/Enums.cs
+++
@@ -1,9 +0,0 @@
-namespace NetGuardGT.Api.Models;
-
-public enum Severity { Low, Medium, High, Critical, Urgent }
-
-public enum IncidentStatus { Registered, Assigned, InProgress, Resolved, Closed }
-
-public enum IncidentType { FiberOptic, Microwave, Electrical, Network, Other }
-
-public enum Specialization { FiberOptic, Microwave, Electrical, Network, General }
